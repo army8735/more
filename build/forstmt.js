09fe6be69a5b6a2119f9fff5bf95306589761bea
army8735 <army8735@qq.com>
2015-03-24 17:45:27 +0800

    @for语句中考虑3个表达式可能为空的情况

--- build/forstmt.js
+++ build/forstmt.js
@@ -4,6 +4,7 @@ var join=function(){var _0=require('./join');return _0.hasOwnProperty("join")?_0
 var ignore=function(){var _1=require('./ignore');return _1.hasOwnProperty("ignore")?_1.ignore:_1.hasOwnProperty("default")?_1.default:_1}();
 var exprstmt=function(){var _2=require('./exprstmt');return _2.hasOwnProperty("exprstmt")?_2.exprstmt:_2.hasOwnProperty("default")?_2.default:_2}();
 var Tree=function(){var _3=require('./Tree');return _3.hasOwnProperty("Tree")?_3.Tree:_3.hasOwnProperty("default")?_3.default:_3}();
+var preVar=function(){var _4=require('./preVar');return _4.hasOwnProperty("preVar")?_4.preVar:_4.hasOwnProperty("default")?_4.default:_4}();
 
 var Token = homunculus.getClass('token', 'css');
 var Node = homunculus.getClass('node', 'css');
@@ -16,8 +17,8 @@ exports.default=function forstmt(node, ignores, index, varHash, globalVar, fnHas
   //忽略掉整个for节点
   ignore(node, ignores, index);
   var temp = node.leaf(3);
-  var isIn = temp.isToken() && temp.content() == 'in';
-  var isOf = !isIn && temp.isToken() && temp.content() == 'of';
+  var isIn = temp && temp.isToken() && temp.token().content() == 'in';
+  var isOf = !isIn && temp && temp.isToken() && temp.token().content() == 'of';
   var s;
   var block;
   var res;
@@ -35,25 +36,44 @@ exports.default=function forstmt(node, ignores, index, varHash, globalVar, fnHas
     temp = ignore(node.leaf(1), ignores, index);
     s += temp.res;
     index = temp.index;
-    //执行for的3个语句，判断是否循环用最后一个
-    exprstmt(node.leaf(2), fnHash, globalFn, varHash, globalVar);
+    //执行for的3个语句，判断是否循环用最后1个
+    //先进行第1个赋值语句，可能为空
+    if(node.leaf(2).name() == Node.VARSTMT) {
+      preVar(node.leaf(2), ignores, index, varHash, globalVar);
+    }
     temp = ignore(node.leaf(2), ignores, index);
     s += temp.res;
     index = temp.index;
-    var loop = exprstmt(node.leaf(3), fnHash, globalFn, varHash, globalVar);
+    //第2个判断语句可能为空，默认条件true
     temp = ignore(node.leaf(3), ignores, index);
     s += temp.res;
     index = temp.index;
-    temp = ignore(node.leaf(4), ignores, index);
+    var loop = true;
+    var lindex = 4;
+    var no2 = true;
+    if(node.leaf(3).name() != Node.TOKEN) {
+      loop = exprstmt(node.leaf(3), fnHash, globalFn, varHash, globalVar);
+      lindex = 5;
+      temp = ignore(node.leaf(4), ignores, index);
+      s += temp.res;
+      index = temp.index;
+      no2 = false;
+    }
+    //第3个循环执行语句也可能为空
+    temp = ignore(node.leaf(lindex), ignores, index);
     s += temp.res;
     index = temp.index;
-    temp = ignore(node.leaf(5), ignores, index);
+    var no3 = true;
+    if(node.leaf(lindex).name() != Node.TOKEN) {
+      lindex++;
+      no3 = false;
+    }
+    //)
+    temp = ignore(node.leaf(lindex), ignores, index);
     s += temp.res;
     index = temp.index;
-    temp = ignore(node.leaf(6), ignores, index);
-    s += temp.res;
-    index = temp.index;
-    block = node.leaf(7);
+    //{block}
+    block = node.leaf(lindex + 1);
     //区分首次循环，后续忽略换行和初始化
     var first = true;
     while(loop) {
@@ -88,8 +108,8 @@ exports.default=function forstmt(node, ignores, index, varHash, globalVar, fnHas
       res += temp.res;
       index = temp.index;
       //判断循环是否继续
-      exprstmt(node.leaf(5), fnHash, globalFn, varHash, globalVar);
-      loop = exprstmt(node.leaf(3), fnHash, globalFn, varHash, globalVar);
+      no3 || exprstmt(node.leaf(lindex - 1), fnHash, globalFn, varHash, globalVar);
+      no2 || (loop = exprstmt(node.leaf(3), fnHash, globalFn, varHash, globalVar));
       first = false;
     }
   }
