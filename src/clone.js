0b5bde665503c007ce86946cc06ab4f24dca14f8
army8735 <army8735@qq.com>
2015-04-07 15:30:48 +0800

    clone改为深clone

--- src/clone.js
+++ src/clone.js
@@ -1,11 +1,30 @@
-function clone(obj, res) {
-  Object.keys(obj).forEach(function(k) {
-    res[k] = obj[k];
-  });
+function isEmptyObject( obj ) {
+  for(var name in obj ) {
+    return false;
+  }
+  return true;
 }
 
-export default function(obj) {
-  var res = {};
-  clone(obj, res);
-  return res;
-};
+function clone(obj) {
+  if(typeof obj != 'object') {
+    return obj;
+  }
+  var re = {};
+  if(obj.constructor == Array) {
+    re = [];
+  }
+  Object.keys(obj).forEach(function(i) {
+    if(!isEmptyObject(obj[i])) {
+      re[i] = clone(obj[i]);
+    }
+    else if(typeof obj[i] != 'object') {
+      re[i] = obj[i];
+    }
+    else {
+      re[i] = {};
+    }
+  });
+  return re;
+}
+
+export default clone;
