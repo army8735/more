da6279748ac61d93267648d78f61d784e4b20310
army8735 <army8735@qq.com>
2015-04-14 15:31:00 +0800

    non-use

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 if(typeof define === 'function' && (define.amd || define.cmd)) {
   define(function(require, exports, module) {
-    require('./fix');
     module.exports = require('./web/More').default;
   });
 }
 else {
-  require('./fix');
   module.exports = require('./build/More').default;
 }
